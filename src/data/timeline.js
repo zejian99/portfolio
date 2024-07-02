01911b726ce95226c63295ee1588c599cc7490b6
Ze Jian <zejiantanwork@gmail.com>
2024-07-02 10:41:13 +0800

    Removed 'Intern' in the timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -14,28 +14,28 @@ export default [
     },
     {
         year: '2023',
-        title: 'Machine Learning Engineer Intern',
+        title: 'Machine Learning Engineer',
         position:'Internship',
         duration: 'Jul 2023 - Dec 2023',
         details: 'Leading the AI Agent integration in CarbonGPT was an exciting challenge. Enhancing platform efficiency with cutting-edge NLP and creating an ML pipeline for geo-spatial analysis revolutionized environmental data interpretation. I loved pushing the boundaries of innovation!'
     },
     {
         year: '2023',
-        title: 'Software Engineer Intern',
+        title: 'Software Engineer',
         position:'Internship',
         duration: 'May 2023 - Jul 2023',
         details: 'Driving backend development for CarbonGPT was a game-changer! We boosted scalability and CI/CD efficiency, cutting deployment times by 80%. Achieving a 5-fold reduction in Docker build times and enhancing product functionality through cross-functional collaboration was super rewarding.'
     },
     {
         year: '2022',
-        title: 'Data Analyst Intern',
+        title: 'Data Analyst',
         position:'Internship',
         duration: 'May 2022 - Jul 2022',
         details: 'I engineered a sophisticated dashboard that boosted productivity by 40% and cut project delivery times by 15% with actionable insights. Innovating tool tracking with a VBA application and advancing tool reliability through comprehensive data analysis was a fantastic experience!'
     },
     {
         year: '2020',
-        title: 'Engineering Intern',
+        title: 'Engineer',
         position:'Internship',
         duration: 'Feb 2020 - Jul 2020',
         details: 'Creating a Python utility adopted by over 100 employees was a huge win for equipment robustness and reliability. Playing a key role in assembling and testing a $1.5 million wafer testing machine showcased my dedication to manufacturing excellence and innovation.'
